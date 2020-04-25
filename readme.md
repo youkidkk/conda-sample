26db018cae7c2da2cdc9edb0a62e29a24ceb6785
youkidkk <youkidkk80@gmail.com>
2020-04-25 13:49:07 +0900

    新規作成

---
+++ readme.md
@@ -0,0 +1,6 @@
+# 環境変数設定
+
+* CONDA_ENVS_PATH  
+  Anaconda 仮想環境ホームのパスを指定
+* CONDA_ENV_DEFAULT  
+  Anaconda 仮想環境名を指定
---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-np.pi, np.pi)
+y = np.sin(x)
+plt.plot(x, y)
+plt.show()
